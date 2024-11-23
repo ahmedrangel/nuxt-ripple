9fd8d98afb8e6d8b301babeec23b372ff8bdb83f
Ahmed Rangel <ahmedrangel@outlook.com>
2024-11-23 08:13:11 -0500

    chore: return computed

--- src/runtime/app/composables.ts
+++ src/runtime/app/composables.ts
@@ -1,7 +1,8 @@
 import type { NuxtRippleRuntimeOptions } from '../../types'
 import { useState } from '#app'
+import { computed } from 'vue'
 
 export function useRipple() {
   const rippleState = useState<NuxtRippleRuntimeOptions>('nuxt-ripple-config')
-  return rippleState
+  return computed(() => rippleState.value)
 }
